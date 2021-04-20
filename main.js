eec3b0d5125a88d6f8bb7a5773895c9049f23c9f
Azat Rahmanov <azat@rahmanow.com>
2021-04-20 10:52:27 +0400

    force push

---
+++ main.js
@@ -0,0 +1,20 @@
+let inter = 1000;
+
+const clock = () => {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const hour = hours * 30;
+    const minute = minutes * 6;
+    const second = seconds * 6;
+    const h = (hours < 10) ? "0" + hours : hours;
+    const m = (minutes < 10) ? "0" + minutes : minutes;
+    document.querySelector('.hours').innerHTML = h;
+    document.querySelector('.mins').innerHTML = m;
+    document.querySelector('.hour').style.transform = `rotate(${hour}deg)`;
+    document.querySelector('.minute').style.transform = `rotate(${minute}deg)`;
+    document.querySelector('.second').style.transform = `rotate(${second}deg)`;
+ }
+
+ setInterval(clock, inter);
