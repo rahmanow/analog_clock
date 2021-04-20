d6746d5187d321a1499f669b0072d7dcd4931f58
Azat Rahmanov <azat@rahmanow.com>
2021-04-21 01:33:58 +0400

    Numbers added. Some CSS fix

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
 let inter = 1000;
-let analogClock = (selector, time) => {
+
+const analogClock = (selector, time) => {
     document.querySelector(selector).style.transform = `rotate(${time}deg)`;
 }
-const clock = () => {
+
+const digitalClock = (selector, showSeconds) => {
     const date = new Date();
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
-    const hour = hours * 30;
-    const minute = minutes * 6;
-    const second = seconds * 6;
     const h = (hours < 10) ? "0" + hours : hours;
     const m = (minutes < 10) ? "0" + minutes : minutes;
-    document.querySelector('.hours').innerHTML = h;
-    document.querySelector('.mins').innerHTML = m;
+    const s = (showSeconds) ? (seconds < 10) ? "0" + seconds : seconds : '';
+    document.querySelector(selector).innerHTML = "<span class=\"hours\">" + h + ":</span><span class=\"mins\">" + m + "</span><span class=\"secs\">" + s + "</span>";
+}
+
+const showDate = (showDate) => {
+    const classes = document.getElementsByClassName('currentDate');
+    return (showDate) ? Object.values(classes).forEach(val => {
+        val.innerHTML = (new Date()).toString().split(' ').splice(1,3).join(' ');
+    }) : '';
+}
+
+const clock = () => {
+    const date = new Date();
+    const hour = date.getHours() * 30;
+    const minute = date.getMinutes() * 6;
+    const second = date.getSeconds() * 6;
     analogClock('.hour', hour);
     analogClock('.minute', minute);
     analogClock('.second', second);
+    digitalClock('.timing', true);
 }
 
-const classes = document.getElementsByClassName('currentDate');
-
-Object.values(classes).forEach(val => {
-    val.innerHTML = (new Date()).toString().split(' ').splice(1,3).join(' ');
-})
-
+showDate(true);
 setInterval(clock, inter);
